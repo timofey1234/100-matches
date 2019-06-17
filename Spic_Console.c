804691c8972643f95c7d82022892435c4b4c0494
timofey1234 <tim.safronov11@gmail.com>
2019-06-17 15:28:05 +0700

    Added

--- Spic_Console.c
+++ Spic_Console.c
@@ -12,6 +12,12 @@ int Count, Num;
 size_t Opponents;
 bool wexist, Correct;
 
+void AITurn(){
+	if (Num>Count){
+		Num=Count;
+	} else Num = rand()%10+1;
+
+}
 
 int main(){
 	srand(time(0));
@@ -22,12 +28,12 @@ int main(){
 
 	while (wexist){
 
-			printf ("Число оппонентов\n");
-			scanf("%d", &Opponents);
-			printf ("Число игроков \n");
-			scanf("%d", &NumOfPlayers);
+			printf ("Number of opponents \n");
+			Opponents = check(Opponents);
+			printf ("Number of players \n");
+			NumOfPlayers = check(NumOfPlayers);
 				if (NumOfPlayers>Opponents) {
-					printf("Упс.Число игроков не может быть больше числа оппонетов\n");
+					printf("Whoops, number of players shouldn't be bigger, than number of opponents \n");
 					NumOfPlayers = Opponents;
 				}
 
@@ -35,13 +41,13 @@ int main(){
 			switch (Player){
 			default: printf ("There's something wrong with this code..\n");
 					break;
-			case 1: printf ("Player %s выйграл \n", name1);
+			case 1: printf ("Player %s is winner \n", name1);
 					break;
-			case 2:	printf ("Player %s выйграл \n", name2);
+			case 2:	printf ("Player %s is winner \n", name2);
 					break;
-			case 3: printf ("Player %s выйграл \n", name3);
+			case 3: printf ("Player %s is winner \n", name3);
 					break;
-			case 4: printf ("Player %s iвыйграл \n", name4);
+			case 4: printf ("Player %s is winner \n", name4);
 					break;
 			}
 	}
